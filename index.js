347f47252a88189208dcf4d32ba931305550a480
anupait <anooprai790@gmail.com>
2021-12-18 18:54:07 +0530

    1st message

---
+++ index.js
@@ -0,0 +1,25 @@
+var cookieParser = require('cookie-parser')
+var csrf = require('csurf')
+var bodyParser = require('body-parser')
+var express = require('express')
+
+var csrfProtection = csrf({ cookie: true })
+var parseForm = bodyParser.urlencoded({ extended: false })
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', csrfProtection,function (req, res) {
+  res.send({ csrfToken: req.csrfToken() });
+})
+
+app.post('/process',parseForm,csrfProtection, function (req, res) {
+ res.send('I am under process route'+ JSON.stringify(req.body));
+});
+
+app.listen(5000, function () {
+  console.log('Listening to Port 5000');
+});
